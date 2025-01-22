71d3a19ae26f825f1a3aed186afbd867eb822ca9
Paul Knapp <ppankluap@gmail.com>
2025-01-21 18:23:19 -0600

    Test: schedule model test with validations and relationships

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Schedule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Relationships" do 
+    it  { should belong_to (:user) }
+    it  { should have_many (:schedule_shows) }
+    it  { should have_many (:shows) }
+  end
+
+  describe "Validations" do 
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:date) }
+  end
 end
